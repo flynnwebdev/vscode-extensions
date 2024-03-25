f3f6e2e20a866a4b75fa2989b1db107fcce08101
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2024-03-25 19:30:40 +1000

    Updated script to handle Windows-only extensions

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+# Read the remote vsc-extensions.txt and install them in *nix environment (MacOS or WSL/Ubuntu)
 for ext in `curl https://raw.githubusercontent.com/flynnwebdev/vscode-extensions/main/vsc-extensions.txt`; do
   code --force --install-extension "$ext"
 done
+
+# If running in WSL, install additional Windows-only extensions
+if type "cmd.exe" | grep WINDOWS > /dev/null; then
+  cmd.exe /C code --force --install-extension "ms-vscode-remote.remote-wsl"
+  cmd.exe /C code --force --install-extension "ms-vscode-remote.remote-wsl"
+fi
